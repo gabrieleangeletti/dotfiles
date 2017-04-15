8d6e0186be07022b79def14ebebe0eee3ad99fdd
Gabriele Angeletti <angeletti.gabriele@gmail.com>
2017-04-15 13:33:00 +0200

    Added create_symlinks.sh

---
+++ create_symlinks.sh
@@ -0,0 +1,10 @@
+# Create symlinks between dotfiles/ and home directory
+cd ~
+
+# zsh
+ln -s ./dotfiles/.oh-my-zsh .oh-my-zsh
+ln -s ./dotfiles/.zshrc .zshrc
+
+# vim
+ln -s ./dotfiles/.vim .vim
+ln -s ./dotfiles/.vimrc .vimrc
